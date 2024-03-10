6fed1cc86d30119d4aecaf2fcad9502715f99b09
Panda29876 <162764250+Panda29876@users.noreply.github.com>
2024-03-10 14:14:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 #Logger Setup
 logger = logging.getLogger(__name__)
 
-TOKEN = "YOUR_TOKEN_HERE"
+TOKEN = "7061390765:AAEjKRIM4mNqVpIQgm4vhwoCjDttxgpUaW8"
 
 def download(update: Update, context: CallbackContext):
     message = update.effective_message
